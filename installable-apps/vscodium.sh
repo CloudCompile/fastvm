d43d0c9d1dbf006b540cbc9488cbee4a71fbb125
Snatch <hausercj77@gmail.com>
2026-09-17 18:30:13 +0000

    some stuff

--- installable-apps/vscodium.sh
+++ installable-apps/vscodium.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -euo pipefail
+install -d -m 0755 /etc/apt/keyrings
 wget -qO /etc/apt/keyrings/vscodium-archive-keyring.gpg \
     https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
 echo "deb [ signed-by=/etc/apt/keyrings/vscodium-archive-keyring.gpg ] https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs vscodium main" \
     > /etc/apt/sources.list.d/vscodium.list
-apt-get update -qq
+chmod 0644 /etc/apt/keyrings/vscodium-archive-keyring.gpg
+apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y codium
 rm -rf /var/lib/apt/lists/*
